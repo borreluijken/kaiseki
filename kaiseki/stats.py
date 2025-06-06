4ec655984e8d947f8851525d100b84c994d50a9a
borreluijken <borre@luijken.com>
2025-06-06 16:15:40 +0100

    initial commit

---
+++ kaiseki/stats.py
@@ -0,0 +1,172 @@
+"""TODO:
+module docstring
+"""
+
+
+from collections import defaultdict
+import heapq
+import math
+import re
+import statistics
+
+from sudachipy import Dictionary, SplitMode
+
+import utils
+
+
+# TODO: vocab_local_freq stores occ not freq
+class Analyzer():
+    """TODO
+    docstring
+    """
+
+    def __init__(self, path):
+        self.vocab_chrono = defaultdict(int)
+        self.vocab_local_freq = defaultdict(int)
+        self.vocab_global_freq = defaultdict(int)
+        self.kanji_chrono = defaultdict(int)
+        self.kanji_local_freq = defaultdict(int)
+        self.kanji_global_freq = defaultdict(int)
+        self.char_count = 0
+        self.vocab_count = 0
+        self.kanji_count = 0
+        self.sentence_count = 0
+        self.unique_vocab = 0
+        self.unique_vocab_once = 0
+        self.unique_kanji = 0
+        self.unique_kanji_once = 0
+        #self.avg_vocab_freq = 0
+        #self.p90_vocab_freq = 0
+        self.p90_kanji_freq = 0
+        self.avg_sentence_len = 0
+        self.kanji_density = 0
+        self.difficulty = 0
+
+        self._tokenizer = Dictionary().create()
+        self._analyze_file(path)
+        self._finalize_stats()
+    
+
+    def _analyze_file(self, path):
+        """TODO: docstring"""
+        for line in utils.file_to_string(path).splitlines():
+            for sentence in utils.split(line):
+                sentence = utils.filter_japanese(sentence)
+                if sentence == '':
+                    continue
+                self._analyze_sentence(sentence)
+    
+
+    def _analyze_sentence(self, sentence):
+        """TODO: docstring"""
+        morphemes = self._tokenizer.tokenize(sentence, SplitMode.A)
+        self.char_count += len(sentence)
+        self.vocab_count += len(morphemes)
+        self.sentence_count += 1
+
+        for morpheme in morphemes:
+            vocab = morpheme.dictionary_form()
+            self.vocab_chrono[vocab] += 1
+            for ch in vocab:
+                if utils.is_kanji(ch):
+                    self.kanji_chrono[ch] += 1
+                    self.kanji_count += 1
+    
+
+    def _finalize_stats(self):
+        """TODO: docstring"""
+        if self.sentence_count == 0:
+            return
+        self.vocab_local_freq = self._sort_freq(True, self.vocab_chrono.items())
+        self.kanji_local_freq = self._sort_freq(True, self.kanji_chrono.items())
+        self.vocab_global_freq = self._sort_freq(False, ((v, utils.get_vocab_freq(v)) for v in self.vocab_chrono))
+        self.kanji_global_freq = self._sort_freq(False, ((k, utils.get_kanji_freq(k)) for k in self.kanji_chrono))
+        self.unique_vocab = len(self.vocab_chrono)
+        self.unique_kanji = len(self.kanji_chrono)
+        self.unique_vocab_once = sum(1 for v in self.vocab_chrono.values() if v == 1)
+        self.unique_kanji_once = sum(1 for k in self.kanji_chrono.values() if k == 1)
+        #self.avg_vocab_freq = round(sum(utils.get_vocab_freq(v) for v in self.vocab_chrono) / self.unique_vocab)
+        #self.p90_vocab_freq = self._calc_percentile_vocab_freq(90)
+        self.p90_kanji_freq = self._calc_percentile_kanji_freq(90)
+        self.avg_sentence_len = round(self.char_count / self.sentence_count, 1)
+        self.kanji_density = round(self.kanji_count / self.char_count, 2)
+        self.difficulty = self._calc_difficulty()
+
+
+    def _sort_freq(self, rev, dic):
+        """Returns a version of the dictionary sorted in ascending order of its values."""
+        return dict(sorted(dic, key=lambda item: item[1], reverse=rev))
+
+
+    def _calc_percentile_vocab_freq(self, percentile):
+        """TODO: docstring"""
+        #freq_values = list(self.vocab_global_freq.values())
+        #index = math.ceil(len(freq_values) * percentile / 100) - 1
+        #return freq_values[index]
+        n = math.ceil(len(self.vocab_global_freq) * percentile / 100)
+        return heapq.nlargest(n, self.vocab_global_freq.values())[-1]
+    
+
+    def _calc_percentile_kanji_freq(self, percentile):
+        """TODO: docstring"""
+        if self.kanji_count == 0:
+            return 0
+        #freq_values = list(self.kanji_global_freq.values())
+        #index = math.ceil(len(freq_values) * percentile / 100) - 1
+        #return freq_values[index]
+        n = math.ceil(len(self.kanji_global_freq) * percentile / 100)
+        return heapq.nlargest(n, self.kanji_global_freq.values())[-1]
+
+
+    def _calc_difficulty(self):
+        """TODO: docstring"""
+        def f(x):
+            res = -(0.00168 * x - 4)
+            res = 1 + math.exp(res)
+            return 10.272 / res
+
+        return round(max(f(self.p90_kanji_freq), 1), 1)
+        # s = max(1, 0.7339 + 0.2094 * self.avg_sentence_len)
+        # v = max(1, 0.00023 * self.avg_vocab_freq)
+        # k = max(1, -2.586 + 30.62 * self.kanji_density)
+        # return round(statistics.geometric_mean([s, v, k]), 1)
+
+
+    def display_stats(self):
+        """TODO: docstring"""
+        print(f"{"Character count":24} {self.char_count:>10,}")
+        print(f"{"Word count":24} {self.vocab_count:>10,}")
+        print(f"{"Kanji count":24} {self.kanji_count:>10,}")
+        print(f"{"Sentence count":24} {self.sentence_count:>10,}")
+        print(f"{"Unique words":24} {self.unique_vocab:>10,}")
+        print(f"{"Unique words used once":24} {self.unique_vocab_once:>10,}")
+        print(f"{"Unique kanji":24} {self.unique_kanji:>10,}")
+        print(f"{"Unique kanji used once":24} {self.unique_kanji_once:>10,}")
+        #print(f"{"Average word frequency":24} {self.avg_vocab_freq:>10,}")
+        #print(f"{"Top 90% word frequency":24} {self.p90_vocab_freq:>10,}")
+        print(f"{"Top 90% kanji frequency":24} {self.p90_kanji_freq:>10,}")
+        print(f"{"Average sentence length":24} {self.avg_sentence_len:>10,}")
+        print(f"{"Kanji density":24} {self.kanji_density:>10.0%}")
+        print(f"{"Difficulty":24} {self.difficulty:>10,}")
+
+
+# DEBUG START
+from pathlib import Path
+base_dir = Path(__file__).resolve().parent.parent
+FSN = base_dir / 'data' / 'vnscripts' / 'fate stay night.txt'
+AOZORA = base_dir / 'data' / 'aozora'
+MANUAL = base_dir / 'data' / 'manual'
+LIGHTNOVELS = base_dir / 'data' / 'light_novels'
+VNSCRIPTS = base_dir / 'data' / 'vnscripts'
+VNSCRIPTS_CORE = base_dir / 'data' / 'vnscripts_core'
+VOCAB_FREQ_CSV = base_dir / 'data' / 'vocab_freq.csv'
+
+# fsn = Analyzer(FSN)
+# fsn.display_stats()
+
+for i in VNSCRIPTS_CORE.iterdir():
+    print(i.name)
+    Analyzer(i).display_stats()
+    print("")
+# DEBUG END
+
---
+++ kaiseki/utils.py
@@ -0,0 +1,104 @@
+"""TODO:
+module docstring
+"""
+
+
+import csv
+import re
+
+from bs4 import BeautifulSoup
+import ebooklib
+from ebooklib import epub
+from pathlib import Path  # DEBUG
+
+
+base_dir = Path(__file__).resolve().parent.parent
+VOCAB_FREQ_CSV = base_dir / 'data' / 'vocab_freq.csv'
+KANJI_FREQ_CSV = base_dir / 'data' / 'kanji_freq.csv'
+
+
+vocab_freq_dict = None
+kanji_freq_dict = None
+
+
+def load_vocab_freq():
+    """Loads vocab frequncy data from kaiseki/data/vocab_freq.csv."""
+    global vocab_freq_dict
+    vocab_freq_dict = {}
+    with open(VOCAB_FREQ_CSV, encoding='utf-8') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            vocab_freq_dict[row[0]] = int(row[1])
+
+
+def load_kanji_freq():
+    """Loads kanji frequncy data from kaiseki/data/kanji_freq.csv."""
+    global kanji_freq_dict
+    kanji_freq_dict = {}
+    with open(KANJI_FREQ_CSV, encoding='utf-8') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            kanji_freq_dict[row[0]] = int(row[1])
+
+
+def filter_japanese(string):
+    """Removes any characters that aren't hiragana, katakana or kanji."""
+    japanese_re = re.compile(r'[\u3040-\u30FF\u4E00-\u9FFF]')
+    return ''.join(japanese_re.findall(string))
+
+
+def is_kanji(char):
+    """Determines whether a character is kanji or not."""
+    return bool(re.search(r'[\u4E00-\u9FFF]', char))
+
+
+def get_vocab_freq(vocab):
+    """Returns the vocab's global frequency."""
+    global vocab_freq_dict
+    if vocab_freq_dict is None:
+        load_vocab_freq()
+    return vocab_freq_dict.get(vocab, 317300)
+
+
+def get_kanji_freq(kanji):
+    """Returns the kanji's global frequency."""
+    global kanji_freq_dict
+    if kanji_freq_dict is None:
+        load_kanji_freq()
+    return kanji_freq_dict.get(kanji, 5526)
+
+
+def split(line):
+    """Splits a line into sentences."""
+    return line.translate({'。':'\n','！':'\n','？':'\n','―':'\n'}).split('\n')
+
+
+# TODO: check if this is working correctly
+def epub_to_string(path):
+    """Extracts and returns all text from an .epub file as a string."""
+    book = epub.read_epub(path)
+    texts = []
+    for item in book.get_items():
+        if item.get_type() == ebooklib.ITEM_DOCUMENT:
+            soup = BeautifulSoup(item.get_body_content(), 'html.parser')
+            texts.append(soup.get_text())
+    return '\n'.join(texts)
+
+
+def txt_to_string(path):
+    """Extracts and returns all text from a .txt file as a string."""
+    with open(path, encoding='utf-8') as f:
+        return f.read()
+
+
+def file_to_string(path):
+    """Extracts and returns all text from the file as a string."""
+    path_obj = Path(path)
+    ext = path_obj.suffix.lower()
+    if ext == '.epub':
+        return epub_to_string(path_obj)
+    if ext == '.txt':
+        return txt_to_string(path_obj)
+    raise ValueError(f"Unsupported file extension: {ext}")
